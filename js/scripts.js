09305064ceff33df00699d7380bf5fc3677249a7
carolmbithe <carombithe97@gmail.com>
2018-08-24 16:49:52 +0300

    use of bootstrap components

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var rolledSide=Math.floor(Math.random()*6)+1
+var totalScore=+
+
+$document
+$("button#play").click(function(){
+  var rolledSide=Math.floor(Math.random()*6)+1
+  var totalScore=+
+
+  if(rolledSide)
+  else()
+}
