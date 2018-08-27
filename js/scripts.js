ad2231dcf97cf46a3b7c26b728a146d9a7d8b557
carolmbithe <carombithe97@gmail.com>
2018-08-27 14:14:58 +0300

    script  changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,28 @@
 
-//function player(rolledside,totalscore) {
-//this.ROLLEDSIDE = rolledside;
-//}
-
-//var player1=new player(rolledside1,totalscore1);
-//var player2=new player(rolledside2,totalscore2);
-//  player.prototype.totalScore = function() {
-  //return this.ROLLEDSIDE += ROLLEDSIDE;
-//}
-//player1.rolledside
-//player2.rolledside
-//player1.totalscore
-//player2.totalscore
-//var totalScore =0;
-
-
+var randomize = function(){
+  var rolledSide = Math.floor((Math.random() * 6) + 1);
+  return rolledSide;
+}
 
 $(document).ready(function() {
 $("button#play1").click(function(){
 
-  var rolledSide1=Math.floor(Math.random()*6)+1
+  var rolledSide1= randomize();
 
   $("#rolledside1").text(rolledSide1);
-  $("#totalscore1").text(rolledSide);
+  var score1 = calcScore(rolledSide1);
+  $("#totalscore1").text(score1);
 
   if(rolledSide1==1  ) {
 
     alert("Ooops!!You rolled 1.It's the other player's turn");
   }
+});
   $("button#hold1").click(function(){
     alert("Its the other players turn");
   });
 $("button#play2").click(function(){
-  var rolledSide2=Math.floor(Math.random()*6)+1
+  var rolledSide2= Math.floor(Math.random()*6)+1
 
 $("#rolledside2").text(rolledSide2);
 $("#totalscore2").text(rolledSide);
@@ -45,3 +35,4 @@ $("button#hold2").click(function(){
 alert("Its the other players turn");
 //}
   });
+});
